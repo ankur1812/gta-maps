d2039b0db3be3f2472977b82d91c53312fe34156
Ankur <ankur.anmol1992@gmail.com>
2018-06-10 18:45:20 -0700

    added MapServices

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ["ngRoute","ui.bootstrap"]);
+var app = angular.module('gtaMaps', ["ngRoute","ui.bootstrap"]);
 app.config(function($routeProvider) {
   $routeProvider
   .when("/", {
--- public/js/controllers/topNavCtrl.js
+++ public/js/controllers/topNavCtrl.js
@@ -1,16 +1,9 @@
 /**
  * Created by aanmol on 6/3/18.
  */
-app.controller('topNavCtrl', function($scope) {
+app.controller('topNavCtrl', function($scope, MapServices) {
     $scope.getMapAreas = function(){
-        var list = [
-            {name: "The Castle", map:'hogwarts', id:'castle'},
-            {name: "Black Lake", map:'hogwarts', id:'lake'},
-            {name: "Quidditch Pitch", map:'hogwarts', id:'quidditch'},
-            {name: "The Forbidden Forest", map:'hogwarts', id:'forbiddenForest'},
-            {name: "Hogsmeade", map:'hogwarts', id:'hogsmeade'},
-
-        ];
+        var list = MapServices.getAreas();
         return list;
     }
 
